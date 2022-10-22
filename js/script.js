47205f0e6f0c84fffa43248e55c574a272fda1b3
husanovjamshid <husanovjamshid11@gmail.com>
2022-10-22 23:35:02 +0500

    finished project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var elBody = document.querySelector('body');
+var elModeBtn = document.querySelector('.buttons__dark');
+
+elModeBtn.addEventListener('click', function () {
+	elBody.classList.toggle('dark');
+});
