e251983a136c9736a44e65e8786f704dc4de5f74
husanovjamshid <husanovjamshid11@gmail.com>
2022-10-27 13:25:51 +0500

    add new photo and change dark mode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var elBody = document.querySelector('body');
-var elModeBtn = document.querySelector('.buttons__dark');
+var elModeBtn = document.querySelector('.dark__mode');
+var elLightBtn = document.querySelector('.light__mode')
 
 
 elModeBtn.addEventListener('click', function () {
-	elBody.classList.toggle('dark');
+	elBody.classList.add('dark');
+	elModeBtn.classList.add('mode__active')
+	elLightBtn.classList.remove('mode__active')
 });
+
+elLightBtn.addEventListener('click', function() {
+	elBody.classList.remove('dark')
+	elModeBtn.classList.remove('mode__active')
+	elLightBtn.classList.add('mode__active')
+})
