3a273cb51bdda4fa23809e4cedb639598b318b8d
husanovjamshid <husanovjamshid11@gmail.com>
2022-10-27 11:53:32 +0500

    change css style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var elBody = document.querySelector('body');
 var elModeBtn = document.querySelector('.buttons__dark');
 
+
 elModeBtn.addEventListener('click', function () {
 	elBody.classList.toggle('dark');
 });
